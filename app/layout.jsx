094eb82f1efd589b903af79975ff98e6edd6a87c
dearaujofelipe <mgdefelipe@outlook.com>
2024-09-24 17:48:56 -0300

    navbar

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,15 +1,19 @@
+import Navbar from '@/components/Navbar';
 import '@/assets/styles/globals.css';
 
 export const metadata = {
   title: 'PropertyPulse | Find the Perfect Rental',
   description: 'Find your dream rental property',
-  keywords: 'renta, find rentals, find properties',
+  keywords: 'rental, find rentals, find properties',
 };
 
 const MainLayout = ({ children }) => {
   return (
     <html lang="eng">
-      <body>{children}</body>
+      <body>
+        <Navbar />
+        <main>{children}</main>
+      </body>
     </html>
   );
 };
